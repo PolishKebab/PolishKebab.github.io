8444af8cde20d8e9b1b70ae9fc9ec208956fa561
unknown <fifik2004@o2.pl>
2022-04-25 18:31:17 +0200

    idk

--- files/authentication.js
+++ files/authentication.js
@@ -1,11 +1,28 @@
+
 window.onload=()=>{
-    const username = prompt("Input username");
-    const password = prompt("Input password");
+    const name = prompt("Input username");
+    const pass = prompt("Input password");
+    function login(name,password){
+        const authInfo = JSON.parse(readFile("data.json"))
+            for(let user in authInfo){
+                const member = authInfo[user]
+                if(member.username==name&&member.password==password){
+                    return true
+                }
+            }
+            return false
+    }
     function readFile(file){
         const fs = new XMLHttpRequest();
         fs.open("GET",`https://polishkebab.github.io/files/${file}`,false)
-        fs.send()
-        if(!fs.responseText.includes("<!DOCTYPE html>")){return fs.responseText}else return "Invalid file"
+            fs.send()
+        if(fs.responseText.includes("<!DOCTYPE"))return `File at https://polishkebab.github.io/files/${file} not found.`
+        return fs.responseText
     }
-    const authInfo = readFile("text.json")
+    const res = document.getElementById("r")
+    if(login(name,pass)){
+        res.innerText=`Welcome ${name},\nThis site is still in progress`
+    }else{
+        res.innerText=`Access denied`
+    }
 }
