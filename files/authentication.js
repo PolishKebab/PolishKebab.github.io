6ab30b4cf1f298e16fa46f20f4e3cbd36cef8acc
unknown <fifik2004@o2.pl>
2022-04-28 22:21:55 +0200

    idk

--- files/authentication.js
+++ files/authentication.js
@@ -1,39 +1,45 @@
 
-window.onload=()=>{
+window.onload=async()=>{
     const button = document.getElementById("subButton")
     const res = document.getElementById("r")
-    button.addEventListener("click",function(){
+    button.addEventListener("click",async function(){
         const username = document.getElementById("username")
         const password = document.getElementById("password")
-        console.log(username.value,password.value)
-        const usr=login(username.value,password.value)
-        console.log(usr)
-        if(usr){
-            localStorage.setItem("accessLvl",usr)
-            res.innerText=`Welcome ${username.value}, this is the admin portal`
-        }else{
+        const usr=await login(username.value,password.value)
+        if(!usr){
             res.innerText="Access Denied"
         }
         username.remove()
         password.remove()
         button.remove()
     })
-    function login(name,password){
-        const authInfo = JSON.parse(readFile("data.json"))
-            for(let user in authInfo){
-                const member = authInfo[user]
-                if(member.username==name&&member.password==password){
-                    console.log(member)
-                    return member.accessLevel
-                }
-            }
-            return null
+    async function login(name,password){
+        const authInfo = [...JSON.parse(readFile("data.json"))]
+        const usr=authInfo.filter(r=>(r.username==name&&r.password==password))[0]
+        if(usr){
+            const userFiles = await loadFiles(name)
+            const container = document.getElementById("files")
+            container.append(document.createElement("span").innerText=userFiles)
+            return usr.accessLevel
+        }
+        return null
     }
     function readFile(file){
         const fs = new XMLHttpRequest();
         fs.open("GET",`https://polishkebab.github.io/files/${file}`,false)
-            fs.send()
+        fs.send()
         if(fs.responseText.includes("<!DOCTYPE"))return `File at https://polishkebab.github.io/files/${file} not found.`
         return fs.responseText
+    }
+    async function loadFiles(user){
+        const response = await fetch(`https://api.github.com/repos/polishkebab/polishkebab.github.io/contents/files/${user}`,{method:"GET"})
+        if(response.status=="404"){
+            return "No user folder found, contact the administrator, if you think this is an error"
+        }
+        const files = JSON.parse(await response.text())
+        if(files.lenght==0){
+            return "No files uploaded"
+        }
+        return files.filter(r=>r.path.startsWith(`files/${user}`)).map(r=>r.path.replace(`files/${user}`,""))
     }
 }
