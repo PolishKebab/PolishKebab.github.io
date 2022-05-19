6cef76e0d5564e00d03d0f67479e34ef7bfdc490
unknown <fifik2004@o2.pl>
2022-05-19 19:32:02 +0200

    More portfolio updates

--- files/portfolio.js
+++ files/portfolio.js
@@ -1,17 +1,33 @@
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
-const optionsList = document.querySelectorAll(".option");
-const desc = document.querySelector(".selected-description")
-desc.style.display="none"
-selected.addEventListener("click", () => {
-  optionsContainer.classList.toggle("active");
-});
 
-optionsList.forEach(o => {
-  o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
-    optionsContainer.classList.remove("active");
-    desc.style.display="block"
-    desc.innerHTML=`Nothing written about ${selected.innerText.bold()} yet`
+window.onload=()=>{
+  const selected = document.querySelector(".selected");
+  const optionsContainer = document.querySelector(".options-container");
+  const optionsList = document.querySelectorAll(".option");
+  const descriptions = document.querySelector(".descriptions")
+  descriptions.style.display="none"
+  const optionContent = document.querySelectorAll(".radio")
+  descriptions.style.display="none;"
+  selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
   });
-});
+  
+  optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+      descriptions.style.display="block"
+      selected.innerHTML = o.querySelector("label").innerHTML;
+      optionsContainer.classList.remove("active");
+      const x = document.querySelectorAll(".description")
+      let selectedOption;
+      for(let x of optionContent){
+        if(o.contains(x))selectedOption=x
+      }
+      if(!selectedOption)return;
+      const description = document.querySelectorAll(`#${selectedOption.id}`)[1]
+      x.forEach(element=>{
+        element.style.display="none"
+      })
+      description.style.display="block"
+    });
+  });
+}
+
