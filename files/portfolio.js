4d5ca7c8bb19fd08d80f7d57b0327e0cbc807a3f
PolishKebab <75347379+PolishKebab@users.noreply.github.com>
2022-12-30 18:29:46 +0100

    Update portfolio.js

--- files/portfolio.js
+++ files/portfolio.js
@@ -1,42 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const select = document.createElement("select");
-  select.classList.add("select");
-  const selectOption = document.createElement("option");
-  selectOption.innerText = "Wybierz repozytorium";
-  selectOption.disabled = true;
-  selectOption.selected = true;
-  select.append(selectOption);
-
-  // Pobierz opcje z API i dodaj je do menu wyboru
-  fetch("https://api.github.com/users/polishkebab/repos")
-    .then((response) => response.json())
-    .then((repos) => {
-      repos.forEach((repo) => {
-        const option = document.createElement("option");
-        option.innerText = repo.name;
-        option.value = repo.name;
-        select.append(option);
-      });
-    });
-
-  // Dodaj obsługę zdarzenia zmiany wyboru w menu wyboru
-  select.addEventListener("change", (event) => {
-    // Pobierz wartość wybranej opcji
-    const repoName = event.target.value;
-    // Pobierz plik README z wybranego repozytorium
-    fetch(`https://api.github.com/repos/polishkebab/${repoName}/contents/README.md`)
+    const select = document.createElement("select");
+    select.classList.add("select");
+    const selectOption = document.createElement("option");
+    selectOption.innerText = "Wybierz repozytorium";
+    selectOption.disabled = true;
+    selectOption.selected = true;
+    select.append(selectOption);
+    // Pobierz opcje z API i dodaj je do menu wyboru
+    fetch("https://api.github.com/users/polishkebab/repos")
       .then((response) => response.json())
-      .then((data) => {
-        // Wyświetl zawartość pliku README w elemencie div o klasie "select-box"
-        const readme = document.querySelector(".select-box");
-        readme.innerHTML = atob(data.content);
+      .then((repos) => {
+        repos.forEach((repo) => {
+          const option = document.createElement("option");
+          option.innerText = repo.name;
+          option.value = repo.name;
+          select.append(option);
+        });
       });
+    select.addEventListener("change", (event) => {
+      const repoName = event.target.value;
+      fetch(`https://api.github.com/repos/polishkebab/${repoName}/contents/README.md`)
+        .then((response) => response.json())
+        .then((data) => {
+          const readme = document.querySelector(".selected");
+          readme.innerHTML = atob(data.content);
+        });
+    });
+    const selectBox = document.querySelector(".select-box");
+    selectBox.style.color = "white";
+    selectBox.append(select)
+    // Ustaw kolor tekstu w elemencie div o klasie "select-box" na biały
   });
-
-  const selectBox = document.querySelector(".select-box");
-  selectBox.insertBefore(select, selectBox.firstChild);
-  select.style.position = "fixed";
-
-  // Ustaw kolor tekstu w elemencie div o klasie "select-box" na biały
-  selectBox.style.color = "white";
-});
