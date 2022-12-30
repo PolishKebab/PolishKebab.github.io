839e5d4369a241b39a6dabca9d45c6f08e372df5
PolishKebab <75347379+PolishKebab@users.noreply.github.com>
2022-12-30 23:21:27 +0100

    Update portfolio.js

--- files/portfolio.js
+++ files/portfolio.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
     selectOption.disabled = true;
     selectOption.selected = true;
     select.append(selectOption);
-    // Pobierz opcje z API i dodaj je do menu wyboru
     fetch("https://api.github.com/users/polishkebab/repos")
       .then((response) => response.json())
       .then((repos) => {
@@ -17,17 +16,29 @@ document.addEventListener("DOMContentLoaded", () => {
           select.append(option);
         });
       });
-    select.addEventListener("change", (event) => {
-      const repoName = event.target.value;
-      fetch(`https://api.github.com/repos/polishkebab/${repoName}/contents/README.md`)
-        .then((response) => response.json())
-        .then((data) => {
-          const readme = document.querySelector(".selected");
-          readme.innerHTML = atob(data.content);
-        });
-    });
+      select.addEventListener("change", event => {
+        const repoName = event.target.value;
+        fetch(`https://api.github.com/repos/polishkebab/${repoName}/contents/README.md`)
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+          })
+          .then(data => {
+            const readme = document.querySelector(".selected");
+            if (data.message) {
+              readme.innerHTML = "";
+            } else {
+              readme.innerHTML = marked.parse(atob(data.content));
+              readme.style.display="block";
+            }
+          })
+          .catch(error => {
+            document.querySelector(".selected").innerHTML="";
+            document.querySelector(".selected").style.display="none";
+          });
+      });
     const selectBox = document.querySelector(".select-box");
     selectBox.style.color = "white";
     selectBox.append(select)
-    // Ustaw kolor tekstu w elemencie div o klasie "select-box" na biały
   });
